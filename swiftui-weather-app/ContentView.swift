a8552ce4abbb7be37fbdc22437235e8dfbb5cce1
ikenna <kennaumeh@gmail.com>
2025-10-02 13:48:51 +0100

    Initial Commit

---
+++ swiftui-weather-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-weather-app
+//
+//  Created by Ikenna Umeh on 02/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-weather-app/swiftui_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_weather_appApp.swift
+//  swiftui-weather-app
+//
+//  Created by Ikenna Umeh on 02/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
